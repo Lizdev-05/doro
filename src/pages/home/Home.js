7cbcf48b268909010296db2936449c43e45244a8
Ojesanmi Elizabeth <ojesanmioyinlade@gmail.com>
2023-11-16 14:34:16 +0000

    Complete about page

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,44 +1,27 @@
 import React from "react";
 import style from "./Home.module.css";
+import homeImg from "../../assets/doro-home-img.svg";
 
 const Home = () => {
   return (
-    <section className={style.inner}>
-      <h1 id="#">Home</h1>
-      <p>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ratione quis
-        animi, eum maxime tempora exercitationem reprehenderit esse tempore
-        nesciunt omnis, ut, possimus voluptatibus sunt sit cupiditate!
-        Necessitatibus deleniti commodi iusto autem itaque, corporis culpa
-        provident soluta! Magnam natus quis consequuntur, ex qui nisi aliquam
-        esse aspernatur repellendus dolorem necessitatibus ipsam. Lorem ipsum
-        dolor sit amet consectetur, adipisicing elit. Ut corrupti eaque quod
-        porro, libero, totam reprehenderit tempore a, eum itaque quam aperiam
-        doloremque eveniet rerum laudantium asperiores? Odit perspiciatis autem
-        quod? Alias voluptatibus officia commodi nesciunt delectus iste est
-        reprehenderit temporibus incidunt quae eaque, sunt magni sed soluta
-        officiis aliquam illum culpa veniam dicta vitae deleniti recusandae illo
-        fugit. Accusantium. Lorem ipsum dolor sit amet consectetur, adipisicing
-        elit. Ut corrupti eaque quod porro, libero, totam reprehenderit tempore
-        a, eum itaque quam aperiam doloremque eveniet rerum laudantium
-        asperiores? Odit perspiciatis autem quod? Alias voluptatibus officia
-        commodi nesciunt delectus iste est reprehenderit temporibus incidunt
-        quae eaque, sunt magni sed soluta officiis aliquam illum culpa veniam
-        dicta vitae deleniti recusandae illo fugit. Accusantium. Lorem ipsum
-        dolor sit amet consectetur, adipisicing elit. Ut corrupti eaque quod
-        porro, libero, totam reprehenderit tempore a, eum itaque quam aperiam
-        doloremque eveniet rerum laudantium asperiores? Odit perspiciatis autem
-        quod? Alias voluptatibus officia commodi nesciunt delectus iste est
-        reprehenderit temporibus incidunt quae eaque, sunt magni sed soluta
-        officiis aliquam illum culpa veniam dicta vitae deleniti recusandae illo
-        fugit. Accusantium. Lorem ipsum dolor sit amet consectetur, adipisicing
-        elit. Ut corrupti eaque quod porro, libero, totam reprehenderit tempore
-        a, eum itaque quam aperiam doloremque eveniet rerum laudantium
-        asperiores? Odit perspiciatis autem quod? Alias voluptatibus officia
-        commodi nesciunt delectus iste est reprehenderit temporibus incidunt
-        quae eaque, sunt magni sed soluta officiis aliquam illum culpa veniam
-        dicta vitae deleniti recusandae illo fugit. Accusantium.
-      </p>
+    <section id="about">
+      <div className={`container ${style.aboutContainer}`}>
+        <div className={style.meContent}>
+          <h1>Building Trust, Preventing Fraud</h1>
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Totam
+            quasi quis, nesciunt neque tenetur a velit et repellat ipsa nemo?
+          </p>
+          <a href="#contact" className="btn btn-primary">
+            Let's talk
+          </a>
+        </div>
+        <div className={style.aboutMe}>
+          <div className={style.meImg}>
+            <img src={homeImg} alt="Home image" />
+          </div>
+        </div>
+      </div>
     </section>
   );
 };
