73b3b3c43dd8da391bd1cba238f87331ad0499da
Ojesanmi Elizabeth <ojesanmioyinlade@gmail.com>
2023-11-16 06:57:08 +0000

    Install react router dom

---
+++ src/component/header/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div>
+      <header>
+        <h2>Logo</h2>
+      </header>
+      <nav></nav>
+    </div>
+  );
+};
+
+export default Header;
