08fd5c89a9a2e1555c6bf61ba5a60a46c1201e5f
Ojesanmi Elizabeth <ojesanmioyinlade@gmail.com>
2023-11-16 09:01:16 +0000

    Complete navbar

--- src/component/header/Header.js
+++ src/component/header/Header.js
@@ -1,20 +1,24 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import style from "./Header.module.css";
 
 const Header = () => {
   return (
     <div>
-      <header>
-        <NavLink to="/">DORO</NavLink>
+      <header className={style.header}>
+        <div>
+          <NavLink to="/">DORO</NavLink>
+        </div>
+
+        <nav className={style.headerRight}>
+          <NavLink to="/about">About</NavLink>
+          <NavLink to="/why-us">Why Us</NavLink>
+          <NavLink to="/trust-profile">Trust Profile</NavLink>
+          <NavLink to="/trust-graph">Trust Graph</NavLink>
+          <NavLink to="/faq">FAQs</NavLink>
+          <NavLink to="/signup">Sign Up</NavLink>
+        </nav>
       </header>
-      <nav>
-        <NavLink to="/about">About</NavLink>
-        <NavLink to="/why-us">Why Us</NavLink>
-        <NavLink to="/trust-profile">Trust Profile</NavLink>
-        <NavLink to="/trust-graph">Trust Graph</NavLink>
-        <NavLink to="/faq">FAQs</NavLink>
-        <NavLink to="/signup">Sign Up</NavLink>
-      </nav>
     </div>
   );
 };
